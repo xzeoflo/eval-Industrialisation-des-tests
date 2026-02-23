0fd63536d56e341ac56be4f9751d40297f28251a
alexkjzz <alex.lam@orange.fr>
2026-02-23 15:05:11 +0100

    feat: test

--- tests/test_exercise2.py
+++ tests/test_exercise2.py
@@ -1,16 +1,23 @@
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
     yield driver
     driver.quit()
-
 def test_extract_full_wiki_news(driver):
     driver.get("https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal")
 
--- tests/test_exercise3.py
+++ tests/test_exercise3.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless") # Obligatoire sur GitHub Actions
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
